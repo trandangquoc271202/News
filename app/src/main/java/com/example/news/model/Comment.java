857cebc9894404c63de3c35d4cf7267ebb18e1b4
unknown <20130376@st.hcmuaf.edu.vn>
2023-05-30 17:45:54 +0700

    cap nhat chưc nang binh luan

---
+++ app/src/main/java/com/example/news/model/Comment.java
@@ -0,0 +1,49 @@
+package com.example.news.model;
+
+public class Comment {
+    private String id;
+    private String content;
+    private String idUser;
+    private String link;
+
+    public Comment(String content, String idUser, String link) {
+        this.content = content;
+        this.idUser = idUser;
+        this.link = link;
+    }
+
+    public Comment() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
